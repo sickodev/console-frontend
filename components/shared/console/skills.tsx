2a22d601fa9725d3abcf71e893c04e25c9fe8360
sickodev <kbishwa03kalyan@gmail.com>
2024-07-16 00:15:56 +0530

    added level to skills section

--- components/shared/console/skills.tsx
+++ components/shared/console/skills.tsx
@@ -5,11 +5,15 @@ import { Progress } from "@/components/ui/progress";
 import { skills } from "@/lib/skills";
 import React from "react";
 import Skill from "./skill";
+import { TypographyP } from "@/components/typography/p";
 
 const Skills = () => {
     return (
         <div className='h-full row-span-1 col-span-1 border bg-black p-1 cursor-default'>
-            <TypographyH4>Skills</TypographyH4>
+            <div className='flex justify-between mx-1'>
+                <TypographyH4>Skills</TypographyH4>
+                <TypographyH4>1</TypographyH4>
+            </div>
             <hr className='border border-green-900' />
             <div className='my-4 px-4 space-y-1'>
                 {skills.map((skill) => (
